5ffa553e1327304a7cf2af7b241e1e4de7b15fd3
Muhammad Shaharyar <muhammadshaharyaraulakh@gmail.com>
2026-01-03 12:15:25 +0500

    Delete

--- admin/handlers/addAdmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/../../config/config.php';
-header('Content-Type: application/json');
-postRequest();
-blockAccess('user','writer','admin');
-$response = createUser($connection, $_POST, 'admin'); 
-echo json_encode($response);
-exit;
